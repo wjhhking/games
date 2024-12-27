10bfdd26d13911da53c58ec9bbef039980acc066
jinius76 <jinwu76@gmail.com>
2024-12-26 21:31:04 -0800

    fix port

--- card_battle/card_battle.py
+++ card_battle/card_battle.py
@@ -71,5 +71,5 @@ def evaluate(socket):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host="0.0.0.0", port=port, debug=True)
